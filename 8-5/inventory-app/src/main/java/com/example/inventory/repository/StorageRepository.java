82704af1578ec05cf8a32c57093c128200ca023c
shunsakukimura <shunkimura325@gmail.com>
2025-12-26 17:21:05 +0900

    Delete 8-5 directory

--- 8-5/inventory-app/src/main/java/com/example/inventory/repository/StorageRepository.java
+++
@@ -1,8 +0,0 @@
-
-package com.example.inventory.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import com.example.inventory.domain.Storage;
-
-public interface StorageRepository extends JpaRepository<Storage, Long> {
-}
